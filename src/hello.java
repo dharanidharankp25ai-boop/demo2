a281b00247b63ca6a530d7afc3af2a568f7822d3
dharanii <dharanidharankp25ai@vetias.ac.in>
2026-06-08 14:54:54 +0530

    Added pass and fail marks logic

--- src/hello.java
+++ src/hello.java
@@ -1,3 +1,12 @@
-void main (){
-    System.out.println("hello vetias");
+void main() {
+    int n = 5;
+    for (int row = 1; row <= n; row++) {
+        for (int col = 1; col <= row; col++) {
+            if (row == 5 && col == 5) {
+                continue;
+            }
+            System.out.print("*");
+        }
+        System.out.println();
+    }
 }
