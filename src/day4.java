448aaed8d3cdd9c880b83f61d85d6abc4c119385
dharanii <dharanidharankp25ai@vetias.ac.in>
2026-06-09 15:58:34 +0530

    add,su, etc

--- src/day4.java
+++ src/day4.java
@@ -1,4 +1,4 @@
-void main() {
+
     /*
     int[][] marks = new int[5][3];
     int[][] StudentMarks = {
@@ -16,6 +16,7 @@ void main() {
     }
 
      */
+      /*
     int[][] attendance = {
             {0, 1, 0},
             {0, 1, 1},
@@ -42,5 +43,48 @@ void main() {
         System.out.println();
     }
 }
+     */
+    /*
+    int[][] attendance = new int[5][3];
+    int[][] StudentAttendance = {
+            {0, 1, 0},
+            {1, 0, 0},
+            {1, 1, 1},
+            {1, 0, 1},
+            {0, 0, 0}
+    };
+    for (int row = 0; row < StudentAttendance.length; row++) {
+        System.out.println("Student" + (row + 1));
+        for (int col = 0; col < StudentAttendance[row].length; col++) {
+            if (StudentAttendance[row][col] == 1)
+                System.out.println("Class : Present");
+            else
+                System.out.println("Class : Absent");
+        }
+        System.out.println();
+    }
+}
+*/
+/*
 
 
+
+void main(){
+    System.out.println(add(4, 5));
+    System.out.println(sub(5, 7));
+    System.out.println(multiply(2, 9));
+    System.out.println(div(4, 8));
+}
+public int add(int a,int b){
+    return a+b;
+}
+public int sub(int a,int b){
+    return a-b;
+}
+public int multiply(int a, int b){
+    return a*b;
+}
+public int div(int a, int b){
+    return a/b;
+}
+*/
