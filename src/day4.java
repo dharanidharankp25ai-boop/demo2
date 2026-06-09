579090d67302c9344dcd80712e48a61302326b62
dharanii <dharanidharankp25ai@vetias.ac.in>
2026-06-09 12:29:30 +0530

    os and 1s

--- src/day4.java
+++ src/day4.java
@@ -1,4 +1,5 @@
 void main() {
+    /*
     int[][] marks = new int[5][3];
     int[][] StudentMarks = {
             {23, 45, 67},
@@ -13,4 +14,33 @@ void main() {
         }
     System.out.println();
     }
+
+     */
+    int[][] attendance = {
+            {0, 1, 0},
+            {0, 1, 1},
+            {1, 0, 1},
+            {1, 1, 0},
+            {0, 0, 1}
+    };
+
+    for (int row= 0; row < attendance.length; row++) {
+        System.out.println("Student " + (row + 1));
+
+        if (attendance[row][0] == 1) {
+            System.out.println("Class: Present");
+        }else{
+            System.out.println("Class : Absent");
+        }
+
+        System.out.println();
+        if (attendance[row][1] == 1) {
+            System.out.println("Class : Present");
+        } else{
+            System.out.println("Class : Absent");
+        }
+        System.out.println();
+    }
 }
+
+
