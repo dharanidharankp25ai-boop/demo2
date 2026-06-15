52d8c91a1b5761c7f373955ca699e45cab11f8c2
dharanii <dharanidharankp25ai@vetias.ac.in>
2026-06-15 09:38:12 +0530

    day 8

--- src/day8.java
+++ src/day8.java
@@ -1,4 +1,7 @@
+
 public class day8 {
+    /*
+
     record CourseRecord(String department, String section){
     }
 
@@ -9,3 +12,51 @@ public class day8 {
 
     }
 }
+ */
+    /*
+    enum language{
+        EN("english"),
+        GR("german"),
+        JP("japanese"),
+        TA("tamil");
+
+        String description;
+
+        language(String description){
+            this.description = description;
+        }
+    }
+
+    static void main() {
+        System.out.println(language.EN.description);
+        System.out.println(language.GR.description);
+        System.out.println(language.JP.description);
+        System.out.println(language.TA.description);
+    }
+}
+
+     */
+    /*
+
+    enum state{
+        TN("tamilnadu"),
+        KA("karnataka"),
+        KL("kerala");
+
+
+        String description;
+
+        state(String description){
+            this.description = description;
+        }
+    }
+
+    static void main() {
+        System.out.println(state.TN.description);
+        System.out.println(state.KA.description);
+        System.out.println(state.KL.description);
+
+    }
+}
+
+     */
