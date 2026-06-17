d541c36e28e305ecb181741d50b6ed8dcb26d0cf
DHARANII <dharanidharankp25ai@vetias.ac.in>
2026-06-17 09:51:00 +0530

    day 10

--- src/day10.java
+++ src/day10.java
@@ -27,6 +27,7 @@ public class day10 {
 }
 
  */
+/*
 public class day10 {
     static void main() {
         LinkedList<String> list = new LinkedList<String>();
@@ -45,4 +46,48 @@ public class day10 {
 
     }
 }
+
+ */
+import java.util.LinkedList;
+
+public class day10{
+
+    static void process(LinkedList<String> list) {
+        System.out.println("Removed : " + list.removeFirst());
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList<String> list = new LinkedList<>();
+
+
+        list.add("101 - ram - 85");
+        list.add("102 - kumar - 78");
+        list.add("103 - ravi - 90");
+        list.add("104 - dharun - 88");
+        list.add("105 - sharan - 75");
+
+        System.out.println("Student List:");
+        System.out.println(list);
+
+
+        list.add("106 - reymanth - 92");
+        System.out.println("\nAfter Adding:");
+        System.out.println(list);
+
+
+        process(list);
+
+        System.out.println("\nAfter Removing:");
+        System.out.println(list);
+
+
+        list.set(2, "104 - Dharun - 95");
+
+        System.out.println("\nAfter Updating:");
+        System.out.println(list);
+    }
+}
+
+
 
